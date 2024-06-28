b69761b9451a94922a2affceabee50ed7d7f7410
JVCRJA <jorgevalenciacalloapaza@gmail.com>
2024-06-28 10:34:57 -0400

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  host: '0.0.0.0',
+server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  },
   head: {
     title: 'POS',
     htmlAttrs: {
