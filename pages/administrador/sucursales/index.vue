5838fb2f88a5cb56ee60beee0dc2474d8ff0a8eb
JVCRJA <jorgevalenciacalloapaza@gmail.com>
2024-07-09 17:33:11 -0400

    Update index.vue

--- pages/administrador/sucursales/index.vue
+++ pages/administrador/sucursales/index.vue
@@ -8,7 +8,6 @@
                <div class="col-12">
                   <div class="card">
                      <div class="card-body">
-<<<<<<< HEAD
                         <nuxtLink :to="url_nuevo" class="btn btn-dark btn-sm w-100 mb-2">
                            <i class="fas fa-plus"></i> Agregar
                         </nuxtLink>
@@ -42,40 +41,6 @@
                               </tbody>
                            </table>
                         </div>
-=======
-                        <table class="table">
-                           <thead>
-                              <tr>
-                                 <th class="py-0 px-1">#</th>
-                                 <th class="py-0 px-1">UBICACION</th>
-                                 <th class="py-0 px-1">CODIGO SUCURSAL</th>
-                                 <th class="py-0 px-1"></th>
-                              </tr>
-                           </thead>
-                           <tbody>
-                              <tr v-for="(m, i) in list" :key="m.id">
-                                 <td class="py-0 px-1">{{ i + 1 }}</td>
-                                 <td class="py-0 px-1">{{ m.ubicacion }}</td>
-                                 <td class="py-0 px-1">{{ m.codigosucursal }}</td>
-                                 <td class="py-0 px-1">
-                                    <div class="btn-group">
-                                       <nuxt-link :to="`${url_editar}${m.id}`" class="btn btn-info btn-sm py-1 px-2">
-                                          <i class="fas fa-pen"></i>
-                                       </nuxt-link>
-                                       <nuxt-link :to="`${url_permisos}${m.id}`"
-                                          class="btn btn-warning btn-sm py-1 px-2">
-                                          <i class="fas fa-key"></i>
-                                       </nuxt-link>
-                                       <button type="button" @click="Eliminar(m.id)"
-                                          class="btn btn-danger btn-sm py-1 px-2">
-                                          <i class="fas fa-trash"></i>
-                                       </button>
-                                    </div>
-                                 </td>
-                              </tr>
-                           </tbody>
-                        </table>
->>>>>>> parent of cffddc1 (04:03)
                      </div>
                   </div>
                </div>
