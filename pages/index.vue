de40552fd3a7943b083fe94ff284003ef3ce9874
JVCRJA <jorgevalenciacalloapaza@gmail.com>
2024-07-09 17:25:38 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,15 @@
             <div v-if="showButton">
                <button>Acción restringida</button>
             </div>
+
+            <div>
+               <h3>Casillas IDs con alquiler activo:</h3>
+               <ul>
+                  <li v-for="casilla in casillasConAlquiler" :key="casilla.casilla_id">
+                     {{ casilla.casilla_id }}
+                  </li>
+               </ul>
+            </div>
          </template>
       </AdminTemplate>
    </div>
@@ -26,6 +35,9 @@ export default {
          modulo: "Dashboard",
          load: false,
          showButton: false,
+         datos: {
+            casillas: [] // Inicializar el arreglo para almacenar las casillas
+         }
       };
    },
    computed: {
@@ -34,6 +46,10 @@ export default {
       },
       sucursalUbicacion() {
          return this.user && this.user.sucursale ? this.user.sucursale.ubicacion : null;
+      },
+      casillasConAlquiler() {
+         // Filtrar las casillas con alquiler_estado: 1
+         return this.datos.casillas.filter(casilla => casilla.casilla_estado === 1);
       }
    },
    methods: {
@@ -41,6 +57,14 @@ export default {
          try {
             const res = await this.$admin.$get(path);
             return res;
+         } catch (error) {
+            console.error(error);
+         }
+      },
+      async CASILLAS(path) {
+         try {
+            const res = await this.$casilla.$get(path);
+            this.datos.casillas = res.casillas;
          } catch (error) {
             console.error(error);
          }
@@ -54,13 +78,16 @@ export default {
       }
    },
    mounted() {
-      this.$nextTick(() => {
+      this.$nextTick(async () => {
+         this.load = true;
          if (process.client && this.user) {
+            await this.CASILLAS('todas-las-casillas'); // Llamar a la función CASILLAS para obtener los datos de las casillas
             this.checkPermissions();
             this.load = false;
          } else {
             this.$router.push('/auth/login');
          }
+
       });
    }
 };
