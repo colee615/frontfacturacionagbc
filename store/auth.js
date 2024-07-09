ced8f24c19daca060b9949486cd1ae84d3ec9a9f
JVCRJA <jorgevalenciacalloapaza@gmail.com>
2024-07-09 14:27:35 -0400

    arreglo 1

--- store/auth.js
+++ store/auth.js
@@ -1,23 +1,27 @@
-// store/auth.js
 export const state = () => ({
   token: null,
   user: null,
+  role: null,
 });
 
 export const mutations = {
   setToken(state, token) {
-
     state.token = token;
   },
   clearToken(state) {
     state.token = null;
   },
   setUser(state, user) {
-
     state.user = user;
   },
   clearUser(state) {
     state.user = null;
+  },
+  setRole(state, role) {
+    state.role = role;
+  },
+  clearRole(state) {
+    state.role = null;
   },
 };
 
@@ -26,6 +30,7 @@ export const actions = {
     if (process.client) {
       const token = localStorage.getItem('token');
       const user = localStorage.getItem('user');
+      const role = localStorage.getItem('role');
 
       if (token) {
         commit('setToken', token);
@@ -34,22 +39,30 @@ export const actions = {
       if (user) {
         commit('setUser', JSON.parse(user));
       }
+
+      if (role) {
+        commit('setRole', role);
+      }
     }
   },
-  login({ commit }, { token, user }) {
+  login({ commit }, { token, user, role }) {
     if (process.client) {
       localStorage.setItem('token', token);
       localStorage.setItem('user', JSON.stringify(user));
+      localStorage.setItem('role', role);
       commit('setToken', token);
       commit('setUser', user);
+      commit('setRole', role);
     }
   },
   logout({ commit }) {
     if (process.client) {
       localStorage.removeItem('token');
       localStorage.removeItem('user');
+      localStorage.removeItem('role');
       commit('clearToken');
       commit('clearUser');
+      commit('clearRole');
     }
   }
 };
--- store/index.js
+++ store/index.js
@@ -1,4 +1,3 @@
-// store/index.js
 export const actions = {
   async nuxtServerInit({ dispatch }) {
     await dispatch('auth/loadAuthFromStorage');
